ac24d3749efe905cc6565f0aa239ac61458fc6ce
Kirill <dr.mokrousoff2017@yandex.ru>
2026-01-22 23:17:08 +0300

    Init

---
+++ Assets/![JAM]MyIndie/!Scripts/Startup.cs
@@ -0,0 +1,14 @@
+using BitterECS.Integration;
+
+public class Startup : EcsUnityRoot
+{
+    protected override void Bootstrap()
+    {
+
+    }
+
+    protected override void PostBootstrap()
+    {
+
+    }
+}
