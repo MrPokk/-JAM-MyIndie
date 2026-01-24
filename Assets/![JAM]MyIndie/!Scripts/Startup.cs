2da4e50f2490be2d233aff31a660cbf427dbf8a8
Petr_l <petr159luda@yandex.ru>
2026-01-24 11:28:14 +0300

    Тесты

--- Assets/![JAM]MyIndie/!Scripts/Startup.cs
+++ Assets/![JAM]MyIndie/!Scripts/Startup.cs
@@ -1,10 +1,11 @@
-using BitterECS.Integration;
+﻿using BitterECS.Integration;
+using UnityEngine;
 
 public class Startup : EcsUnityRoot
 {
     protected override void Bootstrap()
     {
-
+        Debug.Log("adad");
     }
 
     protected override void PostBootstrap()
